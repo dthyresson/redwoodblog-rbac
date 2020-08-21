5243f9c28e8fbff93ea8fa99f99dcca6078fc97f
A. David Thyresson <dthyresson@gmail.com>
2020-08-21 16:02:46 -0400

    Format user component

--- web/src/components/User/User.js
+++ web/src/components/User/User.js
@@ -30,6 +30,28 @@ const User = ({ user }) => {
         <div className="-mt-px flex">
           <div className="w-0 flex-1 flex border-r border-gray-200">
             <a
+              href={`mailto:${user.email}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative w-0 flex-1 inline-flex items-center
+              justify-center py-4 text-sm leading-5 text-gray-700 font-medium
+              border border-transparent rounded-br-lg hover:text-gray-500
+              focus:outline-none focus:shadow-outline-blue focus:border-blue-300
+              focus:z-10 transition ease-in-out duration-150"
+            >
+              <svg
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="mail w-6 h-6"
+              >
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+              </svg>
+              <span className="ml-3">Email</span>
+            </a>
+          </div>
+          <div className="-ml-px w-0 flex-1 flex">
+            <a
               href={`https://app.netlify.com/sites/${process.env.SITE_NAME}/identity/${user.id}`}
               className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
               target="_blank"
@@ -48,22 +70,6 @@ const User = ({ user }) => {
               </svg>
 
               <span className="ml-3">Edit</span>
-            </a>
-          </div>
-          <div className="-ml-px w-0 flex-1 flex">
-            <a
-              href="#"
-              className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
-            >
-              <svg
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="mail w-6 h-6"
-              >
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg>
-              <span className="ml-3">Email</span>
             </a>
           </div>
         </div>
--- web/src/components/UsersCell/UsersCell.js
+++ web/src/components/UsersCell/UsersCell.js
@@ -21,7 +21,10 @@ export const Success = ({ users }) => {
   return (
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {users.map((user) => (
-        <li key={`${user.id}-item`}>
+        <li
+          key={`${user.id}-item`}
+          className="col-span-1 bg-white rounded-lg shadow"
+        >
           <User key={user.id} user={user} />
         </li>
       ))}
