8c172cb50b42be6c95a5e53706e3a69bae47a195
A. David Thyresson <dthyresson@gmail.com>
2020-08-21 13:07:55 -0400

    Call identity users

--- api/src/services/users/users.js
+++ api/src/services/users/users.js
@@ -1,6 +1,22 @@
-export const users = () => {
-  console.log(context)
-  console.log(context.identity)
-  console.log(context.identity.token)
-  return { id: 'afdna' }
+import got from 'got'
+import { requireAuth } from 'src/lib/auth'
+
+export const users = async () => {
+  requireAuth()
+
+  const adminToken = context.clientContext.identity.token
+
+  const { body } = await got.put(
+    'https://${process.env.SITE_NAME}.netlify.app/.netlify/identity/users',
+    {
+      responseType: 'json',
+      headers: {
+        authorization: `Bearer ${adminToken}`,
+      },
+    }
+  )
+  console.log(body)
+
+
+  return { [ {id: 'afdna'} ] }
 }
