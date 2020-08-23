6fb30c1021c5e84f6c996be7d02ff92768b91273
A. David Thyresson <dthyresson@gmail.com>
2020-08-23 12:36:54 -0400

    Test identity triggered functions

--- api/src/functions/identity-signup.js
+++ api/src/functions/identity-signup.js
@@ -1,15 +1,12 @@
 export const handler = async (event, context) => {
-  console.log(event)
-  console.log(event.body)
-  console.log(context)
+  // console.log(event)
+  // console.log(event.body)
+  // console.log(context)
 
-  const adminToken = context.clientContext.identity.token
-  console.log(adminToken)
-
+  // const adminToken = context.clientContext.identity.token
+  // console.log(adminToken)
+  console.log('identity-signup')
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      data: 'identitySignup function',
-    }),
   }
 }
