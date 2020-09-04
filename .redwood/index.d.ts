478b0024ddc37d6a1cf4329d3bb848d10e342a25
A. David Thyresson <dthyresson@gmail.com>
2020-09-04 08:00:58 -0400

    Removes .redwood for gitignore

--- .redwood/index.d.ts
+++
@@ -1,4 +0,0 @@
-// 2020-09-02T05:06:59.195Z
-/// <reference path="./types/import-dir-schemas.d.ts" />
-/// <reference path="./types/import-dir-services.d.ts" />
-/// <reference path="./types/routes.d.ts" />
