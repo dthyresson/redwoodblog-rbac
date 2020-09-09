93b793a8717eda5647ff275944520727baa3e4ac
A. David Thyresson <dthyresson@gmail.com>
2020-09-09 17:16:45 -0400

    Bump to RWJS v18, add signup and About FAQ.

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = (config, { _env }) => {
   config.plugins.forEach((plugin) => {
     if (plugin.constructor.name === 'HtmlWebpackPlugin') {
-      plugin.options.title = 'Redwood Blog'
+      plugin.options.title = 'Redwood Blog with RBAC'
     }
   })
 
