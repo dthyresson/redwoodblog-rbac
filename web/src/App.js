09d0e78bc65ef1c72d8352f3f3f3e3f1631db993
David Thyresson <dthyresson@gmail.com>
2022-11-16 13:56:16 -0500

    Clean up App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,3 @@
-import netlifyIdentity from 'netlify-identity-widget'
-
-import { isBrowser } from '@redwoodjs/prerender/browserUtils'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -12,13 +9,11 @@ import { AuthProvider, useAuth } from './auth'
 import './scaffold.css'
 import './index.css'
 
-isBrowser && netlifyIdentity.init()
-
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
-        <AuthProvider client={netlifyIdentity} type="netlify">
+        <AuthProvider type="netlify">
           <RedwoodApolloProvider useAuth={useAuth}>
             <Routes />
           </RedwoodApolloProvider>
